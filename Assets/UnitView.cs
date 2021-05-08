f02cf34dad53492ef58ceed3af5897d171d97a06
Michail <shipov-mihail@mail.ru>
2021-05-09 00:12:03 +0300

    WIP: Started adding unit factories

--- Assets/UnitView.cs
+++ Assets/UnitView.cs
@@ -4,11 +4,5 @@ using UnityEngine;
 
 public class UnitView : MonoBehaviour
 {
-    private BaseUnit baseUnit;
-    public Health GetHealth => baseUnit.health;
 
-    private void Awake()
-    {
-        baseUnit = new BaseUnit();
-    }
 }
